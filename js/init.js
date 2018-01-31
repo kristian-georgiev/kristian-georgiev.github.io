c06ff68fe976eeacad4160e524f97cb45ca4028f
kristian-georgiev <krisgrg@mit.edu>
2018-01-31 12:42:06 -0500

    minor card ui fix

--- js/init.js
+++ js/init.js
@@ -20,31 +20,4 @@ $('a[href^="#"]').on('click', function(event) {
 
 });
 
-//this is where we apply opacity to the arrow
-$(window).scroll( function(){
-
-  //get scroll position
-  var topWindow = $(window).scrollTop();
-  //multipl by 1.5 so the arrow will become transparent half-way up the page
-  var topWindow = topWindow * 1.5;
-  
-  //get height of window
-  var windowHeight = $(window).height();
-      
-  //set position as percentage of how far the user has scrolled 
-  var position = topWindow / windowHeight;
-  //invert the percentage
-  position = 1 - position;
-
-  //define arrow opacity as based on how far up the page the user has scrolled
-  //no scrolling = 1, half-way up the page = 0
-  $('.arrow-wrap').css('opacity', position);
-
-});
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("nav-mobile").style.display = "none";
-}
-
 
